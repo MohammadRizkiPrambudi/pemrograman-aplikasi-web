e3f70c59fd455c2bfbfcf3544de261b2e3e609d9
MohammadRizkiPrambudi <79072340+MohammadRizkiPrambudi@users.noreply.github.com>
2021-03-08 07:34:48 +0700

    upload latihan menirukkan materi

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+//kita akan membuat sebuah variable dalam php
+$namadepan = "Mohammad";
+$namabelakang = "Rizki Prambudi";
+$namabarang = "Laptop";
+$jumlahbarang = 1;
+$harga = 7000000;
+
+echo "Nama saya adalah $namadepan $namabelakang. Saya membeli $namabarang sebanyak $jumlahbarang seharga $harga";
+
+?>
